5750a058f87a0eca3e2fea4e5e921b7592ae3582
alpt <alpt@4de0e637-9143-440a-99cd-db6a84115eff>
2006-01-04 16:19:45 +0000

    test

--- src/ip_masquerade.sh
+++ src/ip_masquerade.sh
@@ -4,6 +4,8 @@
 # This script is executed by NetsukukuD at its start (if in restricted mode).
 # If all went ok the exit status is 0, otherwise NetsukukuD will stop.
 
+EXTIF="$1"
+
 OS=`uname`
 
 if [ $OS == "Linux" ]
@@ -11,8 +13,24 @@ then
 	# Flush all the NAT rules
 	iptables -F POSTROUTING -t nat  
 
+	# Dynamic IP users:
+	#
+	#   If you get your IP address dynamically from SLIP, PPP, or DHCP, 
+	#   enable this following option.  This enables dynamic-address hacking
+	#   which makes the life with Diald and similar programs much easier.
+	#
+	# echo "1" > /proc/sys/net/ipv4/ip_dynaddr
+
 	# Masquerade
 	iptables -A POSTROUTING -t nat -j MASQUERADE
+	
+	# Static IP users: 
+	#
+	#   If you have a connection with a static IP comment the previous rule
+	#   and use this instead.
+	#
+	# iptables -t nat -A POSTROUTING -o ppp0 -j SNAT --to $INTERNET_IP
+	
 	exit $?
 fi
 
