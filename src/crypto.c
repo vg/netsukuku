2fe02a3f16d47886ceca95a5283edbb9eb6ba896
alpt <alpt@4de0e637-9143-440a-99cd-db6a84115eff>
2006-02-12 11:56:25 +0000

    We don't need to include openssl/engine.h. Why the hell it was included?

--- src/crypto.c
+++ src/crypto.c
@@ -24,7 +24,6 @@
 #include <openssl/evp.h>
 #include <openssl/crypto.h>
 #include <openssl/x509.h>
-#include <openssl/engine.h>
 #include <openssl/err.h>
 #include <openssl/rand.h>
 #include <openssl/rsa.h>
