8b9881b4e21b11f3f961e59f0344145204cfd63c
alpt <alpt@4de0e637-9143-440a-99cd-db6a84115eff>
2006-07-11 23:35:59 +0000

    .

---
+++ site/inc/parse_lang.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if echo "$@" | sed -e 's/\///g' | grep -q '[^a-zA-Z0-9\-\.\ _]'
+then
+	exit 1
+fi
+
+o=`grep "$1" | sed -e "s/$1//" | grep -v '\.info$' | sed -e 's/\.//'`
+
+for i in $o; do
+	printf "<a href=\"index.php?pag=documentation&amp;file=$2$1.$i\">[$i]</a> "
+done
