ad6c287ec3bd2469fc84898bf5a19e21eb0d5a5f
alpt <alpt@4de0e637-9143-440a-99cd-db6a84115eff>
2006-11-08 20:40:32 +0000

    qspn.tex: disjoint routes

--- proto/doc/qspn/tex/make.sh
+++ proto/doc/qspn/tex/make.sh
@@ -1,12 +1,12 @@
 rm -v qspn.{pdf,dvi}
 
-pdflatex --interaction nonstopmode qspn.tex &> /dev/null
-pdflatex --interaction nonstopmode qspn.tex &> /dev/null &&\
-echo PDF compiled
-
 latex  --interaction nonstopmode qspn.tex &> /dev/null
 latex  --interaction nonstopmode qspn.tex &> /dev/null && \
 echo DVI compiled
+
+pdflatex --interaction nonstopmode qspn.tex &> /dev/null
+pdflatex --interaction nonstopmode qspn.tex &> /dev/null &&\
+echo PDF compiled
 
 #latex2html -split 0 -dir html -mkdir qspn.tex &> /dev/null &&\
 #echo HTML compiled
