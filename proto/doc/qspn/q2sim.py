9101497759fd8fb3314ef7d0df8dad6164f2f39b
alpt <alpt@4de0e637-9143-440a-99cd-db6a84115eff>
2006-11-26 22:12:42 +0000

    finished... and debugging

--- proto/doc/qspn/q2sim.py
+++ proto/doc/qspn/q2sim.py
@@ -6,7 +6,8 @@ import sys
 from sys import stdout, stderr
 from string import join
 
-default_rtt=10
+default_rtt=100
+delta_rtt=default_rtt/10
 curtime=0
 events=[]
 
@@ -25,6 +26,11 @@ class packet:
 		self.dst=dst
 		self.me=dst
 		self.tracer=tracer
+		self.splitted_tracer=[]
+		if not tracer:
+			self.first_pkt=1
+		else:
+			self.first_pkt=0
 
 		if dst.id != src.id:
 			delay=self.src.rtt[dst.id]
@@ -32,34 +38,127 @@ class packet:
 			delay=0
 		self.time=curtime+delay
 
-		self.tracer.append(dst)
+		self.tracer.append(self.me)
+		self.split_tracer()
 
+		# put this packet in the event queue
 		heappush(events, self)
 
 	def tracer_to_str(self):
 		return join([i.id for i in self.tracer], '->')
 
+	def split_tracer(self, tp=[]):
+		if not tp:
+			tp=self.tracer
+
+		splitted=0
+		for i,k in enumerate(tp):
+			if i >= 2 and tp[i] == tp[i-2]:
+				self.splitted_tracer.append(tp[:i])
+				self.split_tracer(tp[i-1:])
+				splitted=1
+				break
+		if splitted == 0:
+			self.splitted_tracer.append(tp)
+
+	def evaluate_tracer_packet(self, tp):
+		packet_interesting=0
+		tr=route()
+		old=""
+		for i in reversed(tp):
+			if i == self.me:
+				continue
+			if i == old:
+				AAAAAAAAAAAAAAAAAAAAAAAAAAAA
+				## TODO: continue here
+			old=i
+			tr.append(i)
+
+			iroutes=self.me.get_routes(i.id)
+
+			# Save the route if it is interesting
+			if (tr in iroutes and tr < iroutes[iroutes.index(tr)]) or (tr not in iroutes):
+					self.me.route[i.id].append(tr)
+					packet_interesting=1
+
+		return packet_interesting
+
+	def forward_pkt(self):
+		# forwards to all rnodes
+
+		rlen=len(self.me.rnode_id)
+		if rlen == 1:
+			# erase the outgoing tracer pkt
+			tp=[self.me]
+		else:
+			tp=self.tracer
+
+		for node in self.me.rnode_id:
+			nc=graph.graph[node]
+			if nc == self.src and rlen > 1:
+				continue
+			p=packet(self.me, nc, tp)
+
 	def exec_pkt(self):
-		#
-		# TODO: evaluate the trace of the pkt
-		#
-		tr=[]
-		for i in self.tracer:
-			tr.append(i.id)
-			myroutes=self.get_routes(i.id)
-			### TODO: CONTINUE HERE
+		if not self.first_pkt:
+			#
+			# Evaluate the tracer packet, if it isn't interesting, drop
+			# it, otherwise forward it to the other rnodes.
+			#
+			packet_interesting=0
+			for i in self.splitted_tracer:
+				packet_interesting+=self.evaluate_tracer_packet(i)
 
+			if packet_interesting == 0:
+				return
 
+		# book keeping
 		self.me.tracer.append(self.tracer)
 		error("src: %s, dst: %s"%(self.src.id, self.dst.id))
 		error("\t TP: "+self.tracer_to_str())
-		
-		# forwards to all rnodes
-		for node in self.me.rnode_id:
-			nc=graph.graph[node]
-			if nc == self.src:
-				continue
-			p=packet(self.me, nc, self.tracer)
+
+		self.forward_pkt()
+
+class route:
+	def __cmp__(self, other):
+		if len(self.route) != len(other.route):
+			return -1
+
+		for i,k in zip(self.route, other.route):
+			if i != k:
+				return -1
+
+		if abs(self.trtt-other.trtt) <= delta_rtt:
+			return 0
+		else:
+			return self.trtt-other.trtt
+
+	def compute_trtt(self):
+		self.trtt=0
+		for i,r in enumerate(self.route[:-1]):
+			self.trtt+=r.rtt[self.route[i+1].id]
+
+	def __init__(self, dst=None, route=[]):
+		self.dst=dst
+		self.route=route
+		self.compute_trtt()
+
+	def append(self, node):
+		self.dst=node
+		if not self.route:
+			self.route=[node]
+			self.trtt=0
+		else:
+			self.route.append(node)
+			self.trtt+=self.route[-2].rtt[node.id]
+
+	def extend(self, dst, route):
+		self.dst=dst
+		self.route.extend(route)
+		self.compute_trtt()
+
+	def route_to_str(self):
+		return join([i.id for i in self.route], '->')
 
 class node:
 	total_nodes=0
@@ -85,8 +184,9 @@ class node:
 		return join(ttp, '::')
 
 	def get_routes(self, dst):
-		return [o for i in self.route[dst]]
-	
+		return [i for i in self.route[dst]]
+
+
 class graph:
 	graph={}
 
@@ -128,7 +228,7 @@ class graph:
 	def dump_tracer_packets(self):
 		for id, node in graph.graph.iteritems():
 			error("Node "+id)
-			error("\t"+self.tracer_to_str())
+			error("\t"+node.tracer_to_str())
 
 
 def error(str):
@@ -145,8 +245,8 @@ g.print_dot("test.dot")
 #sys.exit(0)
 
 startnode=graph.graph[random.choice(graph.graph.keys())]
-firstpacket=packet(startnode, startnode)
-firstpacket.exec_pkt()
+first_packet=packet(startnode, startnode)
+first_packet.exec_pkt()
 
 while events:
 	p=heappop(events)
