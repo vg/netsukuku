d1cd6aae743d68b87cb44d966cd51d34ed422546
alpt <alpt@4de0e637-9143-440a-99cd-db6a84115eff>
2007-01-28 07:23:02 +0000

    Added the TOCODE file

--- src/t2.c
+++ src/t2.c
@@ -1,4 +1,27 @@
-#include "t2.h"
+/* This file is part of Netsukuku
+ * (c) Copyright 2007 Andrea Lo Pumo aka AlpT <alpt@freaknet.org>
+ *
+ * This source code is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published 
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This source code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * Please refer to the GNU Public License for more details.
+ *
+ * You should have received a copy of the GNU Public License along with
+ * this source code; if not, write to:
+ * Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * --
+ * tracer.c
+ *
+ * Tracer Packets manipulation functions.
+ */
+
+#include "tracer.h"
 
 /*
  * tp_mask_cmp
