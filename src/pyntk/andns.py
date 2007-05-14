269839cc9094eb60ba3f6c0cdcf736e08437f576
efphe <efphe@4de0e637-9143-440a-99cd-db6a84115eff>
2007-05-14 23:09:24 +0000

    *** empty log message ***

---
+++ src/pyntk/andns.py
@@ -0,0 +1,23 @@
+def query_proto():
+    pass
+
+def query_realm():
+    pass
+
+def dns_to_andna():
+    pass
+
+def dns_forward():
+    pass
+
+def andna_to_dns():
+    pass
+
+def andna_internal():
+    pass
+
+def andns_gethostbyname():
+    pass
+
+def daemonize():
+    pass
