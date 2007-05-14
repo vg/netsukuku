c9a32a00123be8906ea6109707f50f7dc4e894e0
efphe <efphe@4de0e637-9143-440a-99cd-db6a84115eff>
2007-05-14 23:01:50 +0000

    andna interface

---
+++ src/pyntk/andna.py
@@ -0,0 +1,113 @@
+
+#
+## Cache
+#
+
+def load_cache():
+    pass
+
+def dump_cache():
+    pass
+
+def new_cache():
+    pass
+
+#
+## Dialogs
+#
+
+def share_entry():
+    pass
+
+def acquire_entry():
+    pass
+
+def share_db():
+    pass
+
+def acquire_db():
+    pass
+
+def commit_lcache():
+    pass
+
+def register_hname():
+    pass
+
+#
+## Cache
+#
+
+def entry_from_hash():
+    pass
+
+def entry_from_hname():
+    pass
+
+def resolve_hash():
+    pass
+
+def resolve_ip():
+    pass
+
+def is_registered():
+    pass
+
+def registered_entries():
+    pass
+
+def registered_hnames():
+    pass
+
+#
+## Counter Cache
+#
+
+def add_registrant():
+    pass
+
+#
+## Resolved Cache
+#
+
+def rh_add_entry():
+    pass
+
+def rh_entry_from_hash():
+    pass
+
+def rh_entry_from_hname():
+    pass
+
+#
+## Queue
+#
+
+def queue_put():
+    pass
+
+def queue_del():
+    pass
+
+def queue_del_expired():
+    pass
+
+#
+## resolv.conf
+#
+
+def modify_resolv_conf():
+    pass
+
+def restore_resolv_conf():
+    pass
+
+
+#
+## Daemon
+#
+
+def daemonize():
+    pass
+
+
