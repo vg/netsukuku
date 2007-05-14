b300c556b1023c2d9a0207f02066af7b28991ea7
efphe <efphe@4de0e637-9143-440a-99cd-db6a84115eff>
2007-05-14 23:30:00 +0000

    route interface

---
+++ src/pyntk/routing.py
@@ -0,0 +1,30 @@
+def route_add():
+    pass
+
+def route_del():
+    pass
+
+def route_replace():
+    pass
+
+def route_change():
+    pass
+
+def route_append():
+    pass
+
+def route_gw():
+    pass
+
+def node_from_prefix():
+    pass
+
+def route_ip_forward():
+    pass
+
+def route_rp_filter():
+    pass
+
+def route_dev_rp_filter():
+    pass
+
