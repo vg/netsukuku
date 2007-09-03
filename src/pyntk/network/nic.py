90c42a02b95a3e17bd1bdd65f370b08ed0e1b9ff
alpt <alpt@4de0e637-9143-440a-99cd-db6a84115eff>
2007-09-03 14:00:56 +0000

    * nic.py works

--- src/pyntk/network/nic.py
+++ src/pyntk/network/nic.py
@@ -18,20 +18,21 @@
 ##
 
 import commands
-import logging
+import logging as logger
 import re
 
 class Nic:
   """ this class is used to manage network interfaces """
-  def __init__(self, net_device):
-    """ net_device: nic's name """
+  def __init__(self, devname, devaddr=None):
+    """ devname: nic's name """
     
-    self.net_device = net_device
+    self.devname = devname
+    self.devaddr = devaddr
 
   def _exec_ipl(self, ipl_str):
-    """ ipl_str: ip link string (or ip address) """
+    """ ipl_str: ip link string (or ip addr) """
       
-    # execute ip link
+    # execute ip link/addr
     status, output = commands.getstatusoutput(ipl_str)
                 
     # check if everything went good.. if not, log!
@@ -40,34 +41,39 @@ class Nic:
       
     return output
 
-  def nic_up(self):
+  def up(self):
     """ bring up the interface """
-    _ip_link_nic_updown("up")
+    self._ip_link_updown("up")
   
-  def nic_down(self):
+  def down(self):
     """ bring down the interface """
-    _ip_link_nic_updown("down")
+    self._ip_link_updown("down")
     
-  def nic_change_address(self, address):
+  def change_address(self, address):
     """ change the interface's address """
-    _ip_link_nic_change_address(address)
+    self._ip_link_change_address(address)
     
-  def nic_retrieve_info(self, ip_version):
+  def retrieve_info(self, ip_version):
     """ return the following tuple:
         (up_or_down, address)
         up_or_down: True (nic is up) | False (nick is down)
         address: nic's address
     """
-    return _ip_address_show(ip_version)
+    return self._ip_address_show(ip_version)
+
+  def activate(self, address):
+    """ Brings up the interface and changes its address to `address' """
+    self.up()
+    self.change_address(address)
     
   def _ip_address_show(self, ip_version):
-    """ return infos about the interface, obtained via ip address """
+    """ return infos about the interface, obtained via ip addr """
     
-    # generate ip address string
-    ipa_str = "ip address show dev " + str(self.det_device)
+    # generate ip addr string
+    ipa_str = "ip addr show dev " + str(self.devname)
     
-    # execute ip address
-    ipa_out = _exec_ipl(ipa_str)
+    # execute ip addr
+    ipa_out = self._exec_ipl(ipa_str)
     
     # check wether the interface is up or down
     up_reg = re.compile("up", re.IGNORECASE)
@@ -82,7 +88,7 @@ class Nic:
     v6_mat = v6_reg.search(ipa_out)
     if v6_mat is None:
       if ip_version == 6:
-        logger.error("No IPv6 address associated with " + str(self.net_device))
+        logger.error("No IPv6 address associated with " + str(self.devname))
         return (None, None)
 
     # find the ip address
@@ -90,7 +96,7 @@ class Nic:
       ip_reg = re.compile("inet6 (.*:){5}(.*)\/")
       ip_mat = ip_reg.search(ipa_out)
       if ip_mat is None:
-        logger.error("No valid IPv6 address found for " + str(self.net_device))
+        logger.error("No valid IPv6 address found for " + str(self.devname))
         return (None, None)
       else:
         raw_address = ip_mat.group()
@@ -100,7 +106,7 @@ class Nic:
       ip_reg = re.compile("inet (.*\.){3}(.*)\/")
       ip_mat = ip_reg.search(ipa_out)
       if ip_mat is None:
-        logger.error("No valid IPv4 address found for " + str(self.net_device))
+        logger.error("No valid IPv4 address found for " + str(self.devname))
         return (None, None)
       else:
         raw_address = ip_mat.group()
@@ -109,20 +115,34 @@ class Nic:
     
     return (up_or_down, address)
 
-  def _ip_link_nic_up(self, up_or_down):
+  def _ip_link_updown(self, up_or_down):
     """ bring up/down the interface via ip link """
     
     # generate ip link string
-    ipl_str = "ip link set dev " + str(self.net_device) + " " + str(up_or_down)
+    ipl_str = "ip link set dev " + str(self.devname) + " " + str(up_or_down)
     
     # execute ip link
-    _exec_ipl(ipl_str)
+    self._exec_ipl(ipl_str)
 
   def _ip_link_change_address(self, address):
-    """ change the interface's address via ip link """
+    """ change the interface's address via ip addr """
     
-    # generate ip link string
-    ipl_str = "ip link set dev " + str(self.net_device) + " address " + str(address)
+    # flush all the addresses 
+    ipl_str1 = "ip addr flush dev " + str(self.devname)
+    # add the new one
+    ipl_str2 = "ip addr add "+ str(address) +" dev " + str(self.devname)
     
-    # execute ip link
-    _exec_ipl(ipl_str)
+    # execute
+    self._exec_ipl(ipl_str1)
+    self._exec_ipl(ipl_str2)
+
+if __name__ != "main":
+	n=Nic("dummy0")
+	n.up()
+	print n.retrieve_info(4)
+	n.down()
+	print n.retrieve_info(4)
+	n.change_address("11.22.33.44")
+	print n.retrieve_info(4)
+	n.activate("11.22.33.55")
+	print n.retrieve_info(4)
