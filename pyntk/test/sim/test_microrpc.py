aaca1c5321cabbdf2c2bbaf68ae69c2266b5f16c
alpt <alpt@4de0e637-9143-440a-99cd-db6a84115eff>
2007-09-26 17:20:20 +0000

    .

---
+++ pyntk/test/sim/test_microrpc.py
@@ -0,0 +1,167 @@
+# Test suite for rpc.py
+import sys
+import logging
+
+REQUEST = 3
+from random import randint, seed
+PORT =8888
+PORT=randint(8880, 8889)
+
+# Logging option
+
+LOG_LEVEL = logging.DEBUG
+LOG_FILE = ''
+
+log_config = {
+    'level': LOG_LEVEL,
+    'format': '%(levelname)s %(message)s'
+}
+
+if LOG_FILE == '':
+    log_config['stream'] = sys.stdout
+else:
+    log_config['filename'] = LOG_FILE
+
+logging.basicConfig(**log_config)
+
+sys.path.append('../../')
+from ntk.sim.net import Net
+import ntk.sim.sim as sim
+from ntk.sim.wrap.sock import Sock
+from socket import AF_INET, SOCK_DGRAM, SOCK_STREAM, SO_REUSEADDR, SOL_SOCKET
+import ntk.sim.wrap.xtime as xtime
+from ntk.lib.micro import micro, microfunc, allmicro_run
+
+
+seed(1)
+
+N=Net()
+N.net_file_load('net1')
+N.net_dot_dump(open('net1.dot', 'w'))
+
+
+#
+### The server remotable functions
+#
+
+class MyNestedMod:
+    def __init__(self):
+        self.remotable_funcs = [self.add]
+
+    def add(self, x,y): return x+y
+
+class MyMod:
+    def __init__(self):
+        self.nestmod = MyNestedMod()
+
+        self.remotable_funcs = [self.square, self.mul, self.caller_test,
+				self.void_func_caller, self.void_func]
+
+    def square(self, x): return x*x
+    def mul(self, x, y): return x*y
+
+    def private_func(self): pass
+
+    def caller_test(self, _rpc_caller, x, y):
+	c = _rpc_caller
+        logging.debug("caller test: "+str([c.ip, c.port, c.dev, c.socket]))
+	return (x,y)
+    
+    def void_func_caller(self, _rpc_caller):
+	c = _rpc_caller
+        logging.debug("void func caller: "+str([c.ip, c.port, c.dev, c.socket]))
+    def void_func(self):
+	logging.debug("void_func")
+
+mod = MyMod()
+
+
+#
+#### TCP client
+#
+def tcp_client():
+	Sock(N, N.net[1], 'ntk.lib.rpc')
+	import ntk.lib.rpc as rpc
+	from ntk.lib.micro import micro, allmicro_run, micro_block
+        from ntk.network.inet import Inet
+	
+        client = rpc.TCPClient(Inet().ip_to_str(N.net[0].ip), port=PORT)
+   
+        x=5
+        xsquare = client.square(x)
+        assert xsquare == 25
+        xmul7   = client.mul(x, 7)
+        assert xmul7 == 35
+        xadd9   = client.nestmod.add(x, 9)
+        assert xadd9 == 14
+    
+        # something trickier
+        n, nn = client, client.nestmod
+        result = n.square(n.mul(x, nn.add(x, 10)))
+    
+        assert (1,2) == client.caller_test(1,2)
+    
+        try:
+            # should crash now
+            client.private_func()
+        except Exception, e:
+            logging.debug(str(e))
+
+#
+### Bcast client
+#
+def udp_client():
+    Sock(N, N.net[1], 'ntk.lib.rpc')
+    import ntk.lib.rpc as rpc
+    from ntk.lib.micro import micro, allmicro_run, micro_block
+    from ntk.network.inet import Inet
+
+    client = rpc.BcastClient(Inet(), devs=['lo'], port=PORT)
+    print "calling void func"
+    client.void_func()
+    client.void_func_caller()
+    print "udp_client end"
+
+def run_test_tcp():
+    Sock(N, N.net[0], 'ntk.lib.rpc')
+    import ntk.lib.rpc as rpc
+    from ntk.lib.micro import micro, allmicro_run, micro_block
+    from ntk.network.inet import Inet
+
+    print 'Starting tcp server...'
+
+    server = rpc.MicroTCPServer(mod, (Inet().ip_to_str(N.net[0].ip), PORT))
+    micro(server.serve_forever)
+
+    micro(tcp_client)
+    allmicro_run()
+
+def run_test_udp():
+    Sock(N, N.net[0], 'ntk.lib.rpc')
+
+    import ntk.lib.rpc as rpc
+    from ntk.lib.micro import micro, allmicro_run, micro_block
+    from ntk.network.inet import Inet
+
+    print 'Starting udp server...'
+
+    server = rpc.MicroUDPServer(mod, ('', PORT))
+    micro(server.serve_forever)
+
+    micro(udp_client)
+
+
+if __name__ == '__main__':
+  if len(sys.argv) == 1:
+	  print "specify udp or tcp"
+	  sys.exit(1)
+
+  sim.sim_activate()
+
+  if sys.argv[1]== 'tcp':
+	  run_test_tcp()
+  if sys.argv[1]== 'udp':
+	  run_test_udp()
+
+  sim.sim_run()
+  allmicro_run()
