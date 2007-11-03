1e8d1d6f7d243bb20f3db4fd78262d0b948deae8
eriol <eriol@4de0e637-9143-440a-99cd-db6a84115eff>
2007-11-03 02:00:00 +0000

    Reindented. Added return in _ip_route_gen_str

--- pyntk/ntk/network/route.py
+++ pyntk/ntk/network/route.py
@@ -21,199 +21,281 @@ import commands
 import logging
 
 class Route:
-  """ this class is used to manage routes """
+    """ this class is used to manage routes """
 
-  # we're using netsukuku, ain't we?
-  protocol = "ntk"
+    protocol = "ntk"
 
-  def __init__(self, ip_version = 4, multipath=False):
-    """ ip_version: tells if we're using IPv4 or IPv6 """
+    def __init__(self, ip_version = 4, multipath=False):
+        """ ip_version: tells if we're using IPv4 or IPv6 """
 
-    self.ip_version = ip_version
-    self.multipath  = multipath
+        self.ip_version = ip_version
+        self.multipath  = multipath
 
-  def _exec_ipr(self, ipr_str):
-    """ ipr_str: ip route string """
+    def _exec_ipr(self, ipr_str):
+        """ ipr_str: ip route string """
 
-     # execute ip route
-    status, output = commands.getstatusoutput(ipr_str)
+         # execute ip route
+        status, output = commands.getstatusoutput(ipr_str)
 
-    # check if everything went good.. if not, log!
-    if(status != 0):
+        # check if everything went good.. if not, log!
+        if status != 0:
             logging.error(output)
 
-  def _ip_route_gen_str(self, destination_ip = None, destination_bit = None,
-                  protocol = "ntk", table = None, net_device = None, gateway = None):
-    """ destination_ip: destination node's ip address
+    def _ip_route_gen_str(self, destination_ip=None, destination_bit=None,
+                          protocol="ntk", table=None, net_device=None,
+                          gateway=None):
+        """ generate 'ip route'-compatible strings (with a starting space)
+
+        destination_ip: destination node's ip address
         destination_bit: destination node ip's bitmask
         protocol: the protocol we're using
         table: kernel's routing table wehre the route [is | has to be] stored
         net_device: nic we use to get to destination
         gateway: the gateway to use to reach the destination node
-        generate 'ip route'-compatible strings (with a starting space)
-    """
+        """
 
-    # ip route string
-    ipr_str = ""
-    
-    # if an ip is provided, a bitmask has to, also. in this case, use them
-    if(destination_ip != None):
-      if destionation_bit == None:
-        logging.error("Invalid ip provided (missing bitmask)")
-      else:
-        ipr_str += " to " + str(destination_ip) + "/" + str(destination_bit)
-    
-    # if protocol is provided use it
-    if(protocol != None): ipr_str += " protocol " + str(protocol)
+        # ip route string
+        ipr_str = ""
 
-    # if table is provided use it
-    if(table != None): ipr_str += " table " + str(table)
-    
-    # if net_device is provided use it
-    if(net_device != None): ipr_str += " dev " + str(net_device)
-    
-    # if gateway is provided use it
-    if(gateway != None): ipr_str += " via " + str(gateway)
+        # if an ip is provided, a bitmask has to, also. in this case, use them
+        if destination_ip is not None and destionation_bit is None:
+            logging.error("Invalid ip provided (missing bitmask)")
+        else:
+            ipr_str += " to " + str(destination_ip) + "/" + str(destination_bit)
 
-  def _ip_route_add(self, destination_ip, destination_bit, table, net_device, gateway):
-    """ add a route in kernel routing table, via 'ip route' """
+        # if protocol is provided use it
+        if protocol is not None:
+            ipr_str += " protocol " + str(protocol)
 
-    # generate the ip route string
-    ipr_str = "ip route add" + self._ip_route_gen_str(destination_ip, destination_bit, Route.protocol, table, net_device, gateway)
-    
-    # execute ip route
-    self._exec_ipr(ipr_str)
+        # if table is provided use it
+        if table is not None:
+            ipr_str += " table " + str(table)
 
-  def _ip_route_delete(self, destination_ip, destination_bit, table = None, net_device = None, gateway = None):
-    """ delete a route from kernel routing table, via 'ip route' """
+        # if net_device is provided use it
+        if net_device is not None:
+            ipr_str += " dev " + str(net_device)
 
-    # generate the ip route string
-    ipr_str = "ip route delete" + self._ip_route_gen_str(destination_ip, destination_bit, Route.protocol, table, net_device, gateway)
+        # if gateway is provided use it
+        if gateway is not None:
+            ipr_str += " via " + str(gateway)
 
-    # execute ip route
-    self._exec_ipr(ipr_str)
+        return ipr_str
 
-  def _ip_route_flush(self, destination_ip = None, destination_bit = None, table = None, net_device = None, gateway = None):
-    """ flush some routes from kernel routing table via 'ip route' """
+    def _ip_route_add(self,
+                      destination_ip,
+                      destination_bit,
+                      table,
+                      net_device,
+                      gateway):
+        """ add a route in kernel routing table, via 'ip route' """
 
-    # check wether we have at least one valid parameter
-    if((destination_ip == None) and (table == None) and (net_device == None) and (destination == None)):
-      logging.error("Cannot flush without valid criteria")
+        # generate the ip route string
+        ipr_str = "ip route add" + self._ip_route_gen_str(destination_ip,
+                                                          destination_bit,
+                                                          Route.protocol,
+                                                          table,
+                                                          net_device,
+                                                          gateway)
 
-    # generate the ip route string
-    ipr_str = "ip route flush" + self._ip_route_gen_str(destination_ip, destination_bit, Route.protocol, table, net_device, gateway)
+        # execute ip route
+        self._exec_ipr(ipr_str)
 
-    # execute ip route
-    self._exec_ipr(ipr_str)
+    def _ip_route_delete(self,
+                         destination_ip,
+                         destination_bit,
+                         table=None,
+                         net_device=None,
+                         gateway=None):
+        """ delete a route from kernel routing table, via 'ip route' """
 
-  def _ip_route_change(self, destination_ip, destination_bit, table = None, net_device = None, gateway = None):
-    """ change a route in kernel routing table via 'ip route' """
+        # generate the ip route string
+        ipr_str = "ip route delete" + self._ip_route_gen_str(destination_ip,
+                                                             destination_bit,
+                                                             Route.protocol,
+                                                             table,
+                                                             net_device,
+                                                             gateway)
 
-    # generate the ip route string
-    ipr_str = "ip route change" + self._ip_route_gen_str(destination_ip, destination_bit, Route.protocol, table, net_device, gateway)
-    
-    # execute ip route
-    self._exec_ipr(ipr_str)
+        # execute ip route
+        self._exec_ipr(ipr_str)
 
-  def route_add(self, destination_ip, destination_bit, table, net_device, gateway):
-    """ add a route in kernel routing table """
-    self._ip_route_add(destination_ip, destination_bit, table, net_device, gateway)
+    def _ip_route_flush(self,
+                        destination_ip=None,
+                        destination_bit=None,
+                        table=None,
+                        net_device=None,
+                        gateway=None):
+        """ flush some routes from kernel routing table via 'ip route' """
 
-  def route_delete(self, destination_ip, destination_bit, table = None, net_device = None, gateway = None):
-    """ delete a route from kernel routing table """
-    self._ip_route_delete(destination_ip, destination_bit, table, net_device, gateway)
+        # check wether we have at least one valid parameter
+        if ((destination_ip == None) and (table == None) and (net_device == None) and (destination == None)):
+            logging.error("Cannot flush without valid criteria")
 
-  def route_flush(self, destination_ip = None, destination_bit = None, table = None, net_device = None, gateway = None):
-    """ flush some routes from kernel routing table """
-    self._ip_route_flush(self, destination_ip, destination_bit, table, net_device, gateway)
+        # generate the ip route string
+        ipr_str = "ip route flush" + self._ip_route_gen_str(destination_ip,
+                                                            destination_bit,
+                                                            Route.protocol,
+                                                            table,
+                                                            net_device,
+                                                            gateway)
 
-  def route_change(self, destination_ip, destination_bit, table = None, net_device = None, gateway = None):
-    """ change a route in kernel routing table """
-    self._ip_route_change(destination_ip, destination_bit, table, net_device, gateway)
+        # execute ip route
+        self._exec_ipr(ipr_str)
 
-  def route_rp_filter_enable(self, net_device):
-    """ enable rp filtering on net_device """
-    self._route_rp_filter(net_device, True)
+    def _ip_route_change(self,
+                         destination_ip,
+                         destination_bit,
+                         table=None,
+                         net_device=None,
+                         gateway=None):
+        """ change a route in kernel routing table via 'ip route' """
 
-  def route_rp_filter_disable(self, net_device):
-    """ disable rp filtering on net_device """
-    self._route_rp_filter(net_device, False)
-    
-  def route_ip_forward_enable(self):
-    """ enable ip forwarding """
-    self._route_ip_forward(True)
+        # generate the ip route string
+        ipr_str = "ip route change" + self._ip_route_gen_str(destination_ip,
+                                                             destination_bit,
+                                                             Route.protocol,
+                                                             table,
+                                                             net_device, gateway)
 
-  def route_ip_forward_disable(self):
-    """ disable ip forwarding """
-    self._route_ip_forward(False)
+        # execute ip route
+        self._exec_ipr(ipr_str)
 
-  def route_flush_cache(self):
-    """ flush kernel route cache """
-    self._proc_route_flush_cache()
+    def route_add(self,
+                  destination_ip,
+                  destination_bit,
+                  table,
+                  net_device,
+                  gateway):
+        """ add a route in kernel routing table """
+        self._ip_route_add(destination_ip,
+                           destination_bit,
+                           table,
+                           net_device,
+                           gateway)
 
-  def _route_rp_filter(self, net_device, enable):
-    """ enable/disable rp filtering """
-    self._proc_route_rp_filter(net_device, enable)
-    
-  def _route_ip_forward(self, enable):
-    """ enable/disable ip forwarding """
-    self._proc_ip_forward(enable)
+    def route_delete(self,
+                     destination_ip,
+                     destination_bit,
+                     table=None,
+                     net_device=None,
+                     gateway=None):
+        """ delete a route from kernel routing table """
+        self._ip_route_delete(destination_ip,
+                              destination_bit,
+                              table,
+                              net_device,
+                              gateway)
 
-  def _proc_route_rp_filter(self, net_device, enable):
-    """ enable/disable rp filtering via '/proc' """
+    def route_flush(self,
+                    destination_ip=None,
+                    destination_bit=None,
+                    table=None,
+                    net_device=None,
+                    gateway=None):
+        """ flush some routes from kernel routing table """
+        self._ip_route_flush(self,
+                             destination_ip,
+                             destination_bit,
+                             table,
+                             net_device,
+                             gateway)
 
-    # where to write in /proc
-    proc_path = "/proc/sys/net/ipv" + str(self.ip_version) + "/conf/" + str(net_device) + "/rp_filter"
-    
-    # what to write in /proc
-    if(enable):
-      proc_value = "1"
-    else:
-      proc_value = "0"
+    def route_change(self,
+                     destination_ip,
+                     destination_bit,
+                     table=None,
+                     net_device=None,
+                     gateway=None):
+        """ change a route in kernel routing table """
+        self._ip_route_change(destination_ip,
+                              destination_bit,
+                              table,
+                              net_device,
+                              gateway)
 
-    # write in /proc
-    self._proc_write(proc_path, proc_value)
-  
-  def _proc_ip_forward(self, enable):
-    """ enable/disable ip forwarding via '/proc' """
-    
-    # where to write in /proc
-    proc_path = "/proc/sys/net/ipv" + str(self.ip_version)
-    if(self.ip_version == 4):
-      proc_path += "/ip_forward"
-    else:
-      proc_path += "/conf/all/forwarding"
-  
-    # what to write in /proc
-    if enable:
-      proc_value = "1"
-    else:
-      proc_value = "0"
-  
-    # write in proc
-    self._proc_write(proc_path, proc_value)
+    def route_rp_filter_enable(self, net_device):
+        """ enable rp filtering on net_device """
+        self._route_rp_filter(net_device, True)
 
-  def _proc_flush_cache(self):
-    """ flush kernel route cache via '/proc' """
-    
-    # where to write in /proc
-    proc_path = "/proc/sys/net/ipv" + str(self.ip_version) + "/route/flush"
-    
-    # what to write in /proc
-    proc_value = "-1"
-    
-    # write in /proc
-    self._proc_write(proc_path, proc_value)
+    def route_rp_filter_disable(self, net_device):
+        """ disable rp filtering on net_device """
+        self._route_rp_filter(net_device, False)
 
-  def _proc_write(self, proc_path, proc_value):
-    """ write proc_value into proc_path """
-    
-    try:
-      # open proc_path
-      file_handler = open(proc_path, "w")
-      # write
-      file_handler.write(str(proc_value))
-      # close proc_file
-      file_handler.close()
-    except IOError, err:
-      logging.error(err)
+    def route_ip_forward_enable(self):
+        """ enable ip forwarding """
+        self._route_ip_forward(True)
+
+    def route_ip_forward_disable(self):
+        """ disable ip forwarding """
+        self._route_ip_forward(False)
+
+    def route_flush_cache(self):
+        """ flush kernel route cache """
+        self._proc_route_flush_cache()
+
+    def _route_rp_filter(self, net_device, enable):
+        """ enable/disable rp filtering """
+        self._proc_route_rp_filter(net_device, enable)
+
+    def _route_ip_forward(self, enable):
+        """ enable/disable ip forwarding """
+        self._proc_ip_forward(enable)
+
+    def _proc_route_rp_filter(self, net_device, enable):
+        """ enable/disable rp filtering via '/proc' """
+
+        # where to write in /proc
+        proc_path = "/proc/sys/net/ipv" + str(self.ip_version) + "/conf/" + str(net_device) + "/rp_filter"
+
+        # what to write in /proc
+        if(enable):
+            proc_value = "1"
+        else:
+            proc_value = "0"
+
+        # write in /proc
+        self._proc_write(proc_path, proc_value)
+
+    def _proc_ip_forward(self, enable):
+        """ enable/disable ip forwarding via '/proc' """
+
+        # where to write in /proc
+        proc_path = "/proc/sys/net/ipv" + str(self.ip_version)
+        if(self.ip_version == 4):
+            proc_path += "/ip_forward"
+        else:
+            proc_path += "/conf/all/forwarding"
+
+        # what to write in /proc
+        if enable:
+            proc_value = "1"
+        else:
+            proc_value = "0"
+
+        # write in proc
+        self._proc_write(proc_path, proc_value)
+
+    def _proc_flush_cache(self):
+        """ flush kernel route cache via '/proc' """
+
+        # where to write in /proc
+        proc_path = "/proc/sys/net/ipv" + str(self.ip_version) + "/route/flush"
+
+        # what to write in /proc
+        proc_value = "-1"
+
+        # write in /proc
+        self._proc_write(proc_path, proc_value)
+
+    def _proc_write(self, proc_path, proc_value):
+        """ write proc_value into proc_path """
+
+        try:
+            # open proc_path
+            file_handler = open(proc_path, "w")
+            # write
+            file_handler.write(str(proc_value))
+            # close proc_file
+            file_handler.close()
+        except IOError, err:
+            logging.error(err)
