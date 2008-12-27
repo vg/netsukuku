1f0223871a0d8fd74d9709cc15bccdbf58c9d1db
alpt <alpt@4de0e637-9143-440a-99cd-db6a84115eff>
2008-12-27 18:28:14 +0000

    comments

--- pyntk/ntk/sim/inithook.py
+++ pyntk/ntk/sim/inithook.py
@@ -1,3 +1,28 @@
+##
+# This file is part of Netsukuku
+# (c) Copyright 2008 Andrea Lo Pumo aka AlpT <alpt@freaknet.org>
+#
+# This source code is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published 
+# by the Free Software Foundation; either version 2 of the License,
+# or (at your option) any later version.
+#
+# This source code is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# Please refer to the GNU Public License for more details.
+#
+# You should have received a copy of the GNU Public License along with
+# this source code; if not, write to:
+# Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+##
+#
+# Wrap code.
+# First try to load a .py module from sim/. If it fails, try to import a real
+# .py module from ntk/
+#
+
+
 import __builtin__
 import sys
 import imp
