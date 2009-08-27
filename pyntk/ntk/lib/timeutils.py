7ecde392dd629bbf422733e7529b965dbf3d912d
alpt <alpt@4de0e637-9143-440a-99cd-db6a84115eff>
2009-08-27 16:11:35 +0000

    wrapping datetime utils with xtime

--- pyntk/ntk/lib/timeutils.py
+++ pyntk/ntk/lib/timeutils.py
@@ -1,14 +1,16 @@
-import time as T
+import ntk.wrap.xtime as xtime
 from datetime import date, timedelta
 
 def now():
-    return T.time()
+    return xtime.time()
 
 def timestamp_to_data(timestamp):
     return date.fromtimestamp(timestamp)
     
 def today():
-    return date.today()
+    #note: we can't use date.today() because we need to wrap 
+    #      the `time' module with `xtime'.
+    return timestamp_to_data(now())
 
 def days(number=3):
     return timedelta(days=number)
