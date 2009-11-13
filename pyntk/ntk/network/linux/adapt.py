147b99dbc00375b93db2a6974d1c515296995671
lukisi <lukisi@4de0e637-9143-440a-99cd-db6a84115eff>
2009-11-13 17:15:28 +0000

    Fix bug in linux adaptor

--- pyntk/ntk/network/linux/adapt.py
+++ pyntk/ntk/network/linux/adapt.py
@@ -103,9 +103,10 @@ class NIC(BaseNIC):
         # We use only one address for NIC, so a flush is needed.
         if self.address:
             self._flush()
-            current_table = {}
         if address is not None and address != '':
             iproute('addr add %s dev %s' % (address, self.name))
+            global current_table
+            current_table = {}
 
     def get_address(self):
         ''' Gets NIC address. '''
